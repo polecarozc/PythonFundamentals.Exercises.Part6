26a184c43a8925a495b723882552975ec6debd80
Jacob P <jacobp@jacob-p-mbp.mynetworksettings.com>
2023-03-15 21:22:38 -0400

    passed all tests

--- tuple_utils.py
+++ tuple_utils.py
@@ -14,8 +14,7 @@ def tic_tac_toe_finish(board: TicTacToeBoard, pos_y: int, pos_x: int, symbol: st
     :param symbol: The symbol that should be placed in the column (X, or O)
     :return: None
     """
-    pass  # remove pass statement and implement me
-
+    board[pos_y][pos_x] = symbol
 
 def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
     """
@@ -25,7 +24,7 @@ def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
     :param instance: An item in the collection parameter
     :return: An integer.
     """
-    pass  # remove pass statement and implement me
+    return collection.count(instance)
 
 
 def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
@@ -38,8 +37,8 @@ def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
     :param entries: A list or tuple
     :return: None
     """
-    pass  # remove pass statement and implement me
-
+    for index, entry in zip(indexes, entries):
+        print('Index: {0:11} Entry: {1}'.format(str(index), str(entry)))
 
 def print_items_with_index(items: Iterable):
     """
@@ -50,5 +49,5 @@ def print_items_with_index(items: Iterable):
     :param items: A tuple or a list
     :return: None
     """
-    pass  # remove pass statement and implement me
-
+    for (n, item) in enumerate(items, start=1):
+        print('{0}: {1}'.format(n, item))
